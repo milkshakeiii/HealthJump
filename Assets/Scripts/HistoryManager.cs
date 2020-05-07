8e49b3dd2e4ece8168b9dd4e5f900486d0985119
milkshakeiii <milkshakeiii@gmail.com>
2020-05-06 23:33:54 -0700

    slime

---
+++ Assets/Scripts/HistoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryManager : MonoBehaviour
+{
+    private static HistoryManager instance;
+
+    public static HistoryManager GetInstance()
+    {
+        return instance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public int GetHighestReachedPlatformNo()
+    {
+        return 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
